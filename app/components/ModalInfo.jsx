02c21f999619a2fa14063da66bbd700107e99420
aemartos <aemartos@gmail.com>
2018-03-26 09:49:37 +0200

    merge

--- app/components/ModalInfo.jsx
+++ app/components/ModalInfo.jsx
@@ -4,13 +4,14 @@ import Modal from './Modal.jsx';
 export default class ModalInfo extends React.Component {
   constructor(props){
     super(props);
+<<<<<<< Updated upstream
   }
   render(){
     return (
       <Modal show={this.props.show} >
          <div className={"modal-content " + (this.props.show ? "show":"hide")} role="document">
            <div className="modal-header">
-              <button type="button" className="close"  onClick={()=>this.props.handleClose("Info")}><span aria-hidden="true">×</span></button>
+              <button type="button" className="close"  onClick={() => this.props.handleClose("Info")}><span aria-hidden="true">×</span></button>
               <h4 className="modal-title">Instrucciones</h4>
            </div>
            <div className="modal-body">
